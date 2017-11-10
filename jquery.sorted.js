d1fb77015283fd1a111c858e4aa2d8b9f23ae808
Jacek Galanciak <jacek.galanciak@gmail.com>
2017-11-10 16:47:34 +0100

    Reformat code, fix license links

--- jquery.sorted.js
+++ jquery.sorted.js
@@ -5,34 +5,34 @@ jquery.sorted - super simple jQuery sorting utility
 Copyright (c) 2010 Jacek Galanciak
 
 Dual licensed under the MIT and GPL version 2 licenses.
-http://github.com/jquery/jquery/blob/master/MIT-LICENSE.txt
-http://github.com/jquery/jquery/blob/master/GPL-LICENSE.txt
+https://raw.githubusercontent.com/razorjack/quicksand/master/MIT-LICENSE.txt
+https://raw.githubusercontent.com/razorjack/quicksand/master/GPL-LICENSE.txt
 
 Github/docs site: http://github.com/razorjack/jquery.sorted
 
 */
 
 (function($) {
-	$.fn.sorted = function(customOptions) {
+  $.fn.sorted = function(customOptions) {
 
-		var options = {
-			reversed: false,
-			by: function(a) { return a.text(); }
-		};
+    var options = {
+      reversed: false,
+      by: function(a) { return a.text(); }
+    };
 
-		$.extend(options, customOptions);
+    $.extend(options, customOptions);
 
-		$data = $(this);
-		arr = $data.get();
-		arr.sort(function(a, b) {
-			var valA = options.by($(a));
-			var valB = options.by($(b));
-			if (options.reversed) {
-				return (valA < valB) ? 1 : (valA > valB) ? -1 : 0;				
-			} else {		
-				return (valA < valB) ? -1 : (valA > valB) ? 1 : 0;	
-			}
-		});
-		return $(arr);
-	};
+    $data = $(this);
+    arr = $data.get();
+    arr.sort(function(a, b) {
+      var valA = options.by($(a));
+      var valB = options.by($(b));
+      if (options.reversed) {
+        return (valA < valB) ? 1 : (valA > valB) ? -1 : 0;
+      } else {
+        return (valA < valB) ? -1 : (valA > valB) ? 1 : 0;
+      }
+    });
+    return $(arr);
+  };
 })(jQuery);
